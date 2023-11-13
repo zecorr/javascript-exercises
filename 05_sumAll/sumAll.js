029fe8f4a999e185726584a4b2546c4edb427fa4
zecorr <zecorr88@gmail.com>
2023-11-13 18:11:09 -0300

    Complete sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    let first, last
+
+    if (args[0] < 0 || args[1] < 0){return "ERROR"}
+    if (!Number.isInteger(args[0]) || !Number.isInteger(args[1])){return "ERROR"}
+
+    if(args[1] > args[0]){
+        last = args[1]
+        first = args[0]
+    } else if (args [0] > args[1]){
+        last = args[0]
+        first = args[1]
+    }
+    
+    let arr = []
+    while (arr[arr.length - 1] != last){
+        arr.push(first ++)
+    }
+
+    let sum = 0
+    arr.forEach(item => sum += item)
+    return sum
 
 };
 
