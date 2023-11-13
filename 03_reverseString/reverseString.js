73976f49f29b9743497cc661ce1956d95f62b88c
zecorr <zecorr88@gmail.com>
2023-11-13 18:08:26 -0300

    Complete reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitStr = str.split("")
+    let reverseStr = splitStr.reverse()
+    let joinStr = reverseStr.join("")
+    return joinStr
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
