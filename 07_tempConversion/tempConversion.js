d652649be576b4e42d093509754d1e7a52ee1e28
zecorr <zecorr88@gmail.com>
2023-11-13 18:13:14 -0300

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let num = (temp - 32) / 1.8
+  let rounded = Math.round(num * 10) / 10
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let num = (temp * (9/5)) + 32
+  let rounded = Math.round(num * 10) / 10
+  return rounded
+
 };
 
 // Do not edit below this line
