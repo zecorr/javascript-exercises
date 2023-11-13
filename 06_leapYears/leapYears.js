7fcbd82188b1d008db346e3d7bb427e431ee804f
zecorr <zecorr88@gmail.com>
2023-11-13 18:12:06 -0300

    Complete leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return (year % 400 === 0 ? true 
+        : year % 100 === 0 ? false 
+        : year % 4 === 0 ? true
+        : false)
 };
 
 // Do not edit below this line
