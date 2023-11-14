b00c94ca6f8d5ab9063a5e12c5fd5aae8b60cd58
zecorr <zecorr88@gmail.com>
2023-11-14 16:39:43 -0300

    Complete calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,19 @@
-const add = function() {
-	
+const add = (a, b) => a + b
+
+const subtract = (a, b) => a - b
+
+const sum = num => num.reduce((acc, curr) => acc + curr, 0);
+
+const multiply = num => num.reduce((acc, curr) => acc * curr, 1)
+
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = num => {
+  if (num < 0) return -1;
+  else if (num == 0 ) return 1;
+  else {return (num * factorial(num -1))}
 };
 
 // Do not edit below this line
