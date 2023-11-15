ece051757fb52e94f91cba43e6f332ed261b1753
zecorr <zecorr88@gmail.com>
2023-11-15 12:10:59 -0300

    Complete fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const arr = [1, 1]
+    let newNum
+    if (num < 1) return "OOPS"
+    else if(num == 1) return 1
+    else if (num == 2) return 1
+    else {
+        while(arr.length < num){
+            newNum = arr[arr.length - 1] + arr[arr.length - 2]
+            arr.push(newNum)
+        }
+    }
+    return newNum
 };
 
 // Do not edit below this line
