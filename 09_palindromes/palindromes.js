b27710bd2ff2ff965b13e65375818c80b5fa74ac
zecorr <zecorr88@gmail.com>
2023-11-15 10:29:06 -0300

    Complete palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const regex = /\W/ // filter non word characters
+    const forward = str.split(regex).join('').toLowerCase()
+    const reverse = str.split(regex).join('').split('').reverse().join('').toLowerCase()
+    return forward === reverse
 };
 
 // Do not edit below this line
