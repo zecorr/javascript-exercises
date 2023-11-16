6335a3f5df625d2a5c6083e4311bcf56c921bb0b
zecorr <zecorr88@gmail.com>
2023-11-16 12:44:53 -0300

    Complete findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const ages = []
+    // Push ages of all people into ages array
+    arr.forEach( obj => {
+        if(obj.yearOfDeath == undefined){obj.yearOfDeath = new Date().getFullYear()} // add current year if still alive
+        ages.push(obj.yearOfDeath - obj.yearOfBirth) // calculate current age in years and push to ages array
+    })
+    
+    // Copy and sort contents of ages array because sort() will manipulate the original ages array
+    newAges = [...ages] // now only newAges array gets manipulated after copying everything from ages array
+    const sorted = newAges.sort((a, b) => a - b) // sort numerically 
+    const highest = sorted[sorted.length - 1] // get highest age
 
+    // Get index number from oldest person
+    let newIndex
+    ages.forEach((age, index) => {
+        if(age == highest){newIndex = index}
+    })
+    return arr[newIndex]
 };
 
 // Do not edit below this line
